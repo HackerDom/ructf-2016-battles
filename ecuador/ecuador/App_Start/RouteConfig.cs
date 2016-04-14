75f7ffb4213924824d0d3e267b456d812bb82446
Polina Zonova <polinas@skbkontur.ru>
2016-04-15 00:37:19 +0500

    +ecuador task

---
+++ ecuador/ecuador/App_Start/RouteConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ecuador
+{
+	public class RouteConfig
+	{
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+			routes.RouteExistingFiles = false;
+
+			routes.MapRoute(
+				name: "Default",
+				url: "",
+				defaults: new { controller = "Home", action = "Index" }
+			);
+		}
+	}
+}
---
+++ ecuador/ecuador/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ecuador.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			const string flag = "Pr0xysAreAwes0me";
+			var request = HttpContext.Request;
+			if (IsProperIp(request.UserHostAddress))
+				return Content(String.Format("Your IP is {0} and here is your flag, buddy: {1}", request.UserHostAddress, flag));
+			return Content(String.Format("Your IP is: {0} and this content is not available in your country", request.UserHostAddress));
+		}
+
+		private bool IsProperIp(string userHostAddress)
+		{
+			var russianIps = System.IO.File.ReadAllLines(Path.Combine(HttpRuntime.AppDomainAppPath,"ips.txt"));
+			var parts = userHostAddress.Split('.');
+			if (parts.Length > 0)
+				if (russianIps.Select(ip => ip.Trim()).Contains(parts[0].Trim()))
+					return false;
+				else
+				{
+					return true;
+				}
+			return false;
+		}
+	}
+}
